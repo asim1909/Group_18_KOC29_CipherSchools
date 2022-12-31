951a22bce64321314d74b75bdb4c1d1467a3154b
出久 <118390636+asim1909@users.noreply.github.com>
2022-12-31 11:11:31 +0530

    Update Time-Angle beta.py

--- Time-Angle beta.py
+++ Time-Angle beta.py
@@ -10,13 +10,12 @@ def calcAngle(h,m):
         h += 1;
     if(h>12):
         h = h-12;
-
+   
     hour_angle = 0.5 * (h * 60 + m)  # Calculating the angles moved by hour and minute hands with reference to 12:00
     minute_angle = 6 * m
     angle =(hour_angle - minute_angle)     # Find the difference between two angles
     c=abs(360-abs(angle))
     angle = min(c, abs(angle))       # Return the smaller angle of two possible angles
     return angle
-h=int(input("Hour: "))
-m=int(input("Minute: "))
+h,m=map(int, input("Please enter the time ").split())
 print('Angle ', calcAngle(h,m),"°")
